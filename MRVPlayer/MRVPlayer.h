cf377e68ed898d36855659ce00605570f28c1737
Mukilarasan Ravi <mukilarasan.ravi@gmail.com>
2023-06-11 15:51:03 +0530

    added frameworks files

---
+++ MRVPlayer/MRVPlayer.h
@@ -0,0 +1,18 @@
+//
+//  MRVPlayer.h
+//  MRVPlayer
+//
+//  Created by Mukilarasan R on 11/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MRVPlayer.
+FOUNDATION_EXPORT double MRVPlayerVersionNumber;
+
+//! Project version string for MRVPlayer.
+FOUNDATION_EXPORT const unsigned char MRVPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MRVPlayer/PublicHeader.h>
+
+
